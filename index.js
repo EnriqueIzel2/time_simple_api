c3d667bfe7b524e779d9819a1a4b41974abf00f8
EnriqueIzel2 <eleaoizel@gmail.com>
2022-08-04 23:01:41 -0400

    feat: add cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const server = express();
 const port = process.env.PORT || 3000;
 
 const dateService = require("./src/dateService");
+server.use(cors());
+server.use(express.json());
 
 server.get("/dateTime", (req, res) => {
   return res.json(dateService.getCurrentTime());
